403c63415c023eab86391338c7c6f56183ce2bdb
Georgy Gusev <goosebumpsmail2@gmail.com>
2020-02-19 23:57:19 +0300

    Перевел php в html, проверка текста на цифры, нормальная проверка языка

--- input-handler.js
+++ input-handler.js
@@ -1,33 +1,61 @@
 $('#buttonStart').click(function () {
 
-    let openText = $('#openText').text();
-    let key = $('#key').text();
-    let cipherText = $('#cipherText').text();
-    
+    let openText = $('#openText').val();
+    let key = $('#key').val();
+    let cipherText = $('#cipherText').val();
+
     if (openText.length > 0)
-        openToCrypt(lang, openText, key);
+        openToCrypt(key, openText, languageTest(openText));
     else
-        cryptToOpen(lang, cipherText, key);
+        cryptToOpen(key, cipherText, languageTest(cipherText));
 
 
-})
+});
+
+$('#openText').keyup(function () {
+
+    $(this).val(deleteNum($(this).val()));
+
+});
+
+$('#cipherText').keyup(function () {
+
+    $(this).val(deleteNum($(this).val()));
+
+});
+
+function deleteNum(text) {
+
+    return text.replace(/[0-9]/g, '');
+
+}
+
 
 function languageTest(text) {
 
-    let pattern = '/[а-яА-Я]/gu';
+    if (text.search(/[а-яА-Я]+/gu) !== -1 && text.search(/[a-zA-Z]+/gu) !== -1) {
 
-    if (text.match(pattern)[0].length > 0)
-        return 'ru';
-    else
+        alert('Некорректный ввод');
+        throw new Error('Некорректный ввод. Встретились два типа языка');
+
+    }
+
+    if (text.search(/[a-zA-Z]+/gu) !== -1)
         return 'en';
+    else
+        return 'ru';
 
 
 }
 
-function openToCrypt(lang, text, key) {
+function openToCrypt(key, text, lang) {
+
+    console.log(vizhener.encryption(lang, text, key));;
 
 }
 
-function cryptToOpen(lang, text, key) {
+function cryptToOpen(key, text, lang) {
+
+    console.log(vizhener.decryption(lang, key, text));
 
 }
